38777a7b78330df5332d91d777f054f780d914a5
Yuta <yuuta.dev@gmail.com>
2022-11-16 13:17:29 +0200

    updates

--- lab-3-a/location.js
+++ lab-3-a/location.js
@@ -5,11 +5,12 @@ function on_success(position) {
 	document.getElementById('acc').innerHTML = 'Accuracy = ' + position.coords.accuracy
 	document.getElementById('altitude').innerHTML = 'Altitude = ' + position.coords.altitude
 	const latlon = position.coords.latitude + ',' + position.coords.longitude
+	const tempApi = 'AIzaSyDi8lxp3yL8gC7YiHcxKz4xVMueQOGfsEo'
 	const img_url =
 		'https://maps.googleapis.com/maps/api/staticmap?center=' +
 		latlon +
 		'&zoom=14&size=400x300&key=' +
-		API_KEY
+		tempApi
 	document.getElementById('map').innerHTML = "<img id='map-img' src='" + img_url + "'>"
 }
 function on_error(e) {
